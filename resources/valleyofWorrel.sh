3eea509fb122b02833ffaa5d318531639307202e
hubofvalley <hubofvalley@users.noreply.github.com>
2026-09-09 19:52:52 +0700

    fix: repin Worrel launcher helpers

--- resources/valleyofWorrel.sh
+++ resources/valleyofWorrel.sh
@@ -18,7 +18,7 @@ readonly VALLEY_INSTALLER_SHA256="e82df5a21bf03abc763fca89bc4b5a33831f6369248a26
 readonly VALLEY_UPDATER_SHA256="07ceef513c3acc65c6a4efa6540f92bf037ce66b16d524f424ca2c07e55a1b70"
 readonly VALLEY_COSMOVISOR_MIGRATION_SHA256="c37898ad62f0cd8b031cfc4a19b129473ab56a457cb2ca4a3d5da32fa6334d90"
 readonly VALLEY_COSMOVISOR_UPGRADE_SHA256="68414d1792a1f5bde935a5a1e9c14660a881b185ed5b95a199a9a68bb76a72a7"
-readonly VALLEY_SCRIPT_BASE="https://raw.githubusercontent.com/hubofvalley/Valley-of-Worrel-Testnet/143e7db6c5aa7deccd2a7bab14a08be6666ff77f/resources"
+readonly VALLEY_SCRIPT_BASE="https://raw.githubusercontent.com/hubofvalley/Valley-of-Worrel-Testnet/bf042c356521846253b6791c302161d81d2a107f/resources"
 
 LOGO=''
 LOGO+=' __      __                    _ _               _\n'
